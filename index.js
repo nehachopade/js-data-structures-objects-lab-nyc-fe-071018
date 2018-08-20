ab8f4cc7224c99280634518ed727cf82ca2ee2ee
Neha Chopade <nehachopade22@gmail.com>
2018-08-20 17:31:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function updateDriverWithKeyAndValue(driver, key, value)
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
-{  driver.key=value;
+{  driver[key]=value;
    return driver;
   
 }
