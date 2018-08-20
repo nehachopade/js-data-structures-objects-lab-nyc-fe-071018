31acc11e674a79d109bda7c6eca357acf8d64ef7
Neha Chopade <nehachopade22@gmail.com>
2018-08-20 17:57:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const driver = {
+  firstName:'Neha',
+  lastName:'Chopade',
+  age:25,
+  city:'Delhi'
+}
+
+function updateDriverWithKeyAndValue(driver, key, value)
+{
+  const copy = Object.assign ({}, driver,{[key]:value});
+  return copy;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
+{  driver[key]=value;
+   return driver;
+  
+}
+function deleteFromDriverByKey(driver,key) 
+{
+  const copy = Object.assign({},driver);
+  delete copy[key];
+  return copy;
+}
+function destructivelyDeleteFromDriverByKey(driver, key){
+  delete driver[key];
+  return driver;
+}
+
+
+
+
+
