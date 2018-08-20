5d1d5ac79dbe24c101343b2b04d51e972bbe3403
Neha Chopade <nehachopade22@gmail.com>
2018-08-20 17:34:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,8 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
 }
 function deleteFromDriverByKey(driver, key) 
 {
-  delete driver.key ;
+  const copy = Object.assign({}, driver);
+  delete copy.key;
+  return copy;
+  
 }
